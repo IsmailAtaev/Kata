8ef83a91aaf2e4ff69f453478c619133b151830e
IsmailAtaev <issyataew@gmail.com>
2022-01-30 04:03:33 +0300

    task 2.1

--- src/main/java/com/ataev/test_kata/KataTest.java
+++ src/main/java/com/ataev/test_kata/KataTest.java
@@ -11,7 +11,7 @@ public class KataTest {
 
     public static int getCount(String str) {
         int vowelsCount = 0;
-        if(str != null) {
+        if (str != null) {
             for (int i = 0; i < str.length(); i++) {
                 if (str.charAt(i) == 'a' || str.charAt(i) == 'e' || str.charAt(i) == 'i' ||
                         str.charAt(i) == 'o' || str.charAt(i) == 'u') {
@@ -21,14 +21,21 @@ public class KataTest {
         }
         return vowelsCount;
     }
+
     public static int getCount1(String str) {
-        return str.replaceAll("[^aeiou]","").length();
+        return str.replaceAll("[^aeiou]", "").length();
     }
+
     public static int getCount3(String str) {
-        return (int)str.chars().mapToObj(i -> (char)i).filter(i -> "aeiou".contains(String.valueOf(i))).count();
+        return (int) str.chars().mapToObj(i -> (char) i).filter(i -> "aeiou".contains(String.valueOf(i))).count();
     }
 
     public static int getCount4(String str) {
         return (int) str.chars().filter(c -> "aeiou".indexOf(c) >= 0).count();
     }
+
+    public int rowSumOddNumbers(int n) {
+        return n * n * n;
+    }
+
 }
