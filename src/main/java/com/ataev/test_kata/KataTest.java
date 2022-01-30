271d0f2df1f9d493a4c9f1d30d2339f31f98f1a6
IsmailAtaev <issyataew@gmail.com>
2022-01-30 03:45:26 +0300

    task 1.1

---
+++ src/main/java/com/ataev/test_kata/KataTest.java
@@ -0,0 +1,24 @@
+package com.ataev.test_kata;
+
+import java.util.ArrayList;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class KataTest {
+    public static void main(String[] args) {
+        System.out.println(getCount(null));
+
+    }
+
+    public static int getCount(String str) {
+        int vowelsCount = 0;
+        if(str != null) {
+            for (int i = 0; i < str.length(); i++) {
+                if (str.charAt(i) == 'a' || str.charAt(i) == 'e' || str.charAt(i) == 'i' ||
+                        str.charAt(i) == 'o' || str.charAt(i) == 'u') {
+                    ++vowelsCount;
+                }
+            }
+        }
+        return vowelsCount;
+    }
+}
