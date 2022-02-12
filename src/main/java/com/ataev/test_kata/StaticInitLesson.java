98b5131172b1936d410270b4eec6a27d13ce84bc
IsmailAtaev <ataewisma@gmail.com>
2022-02-12 23:58:43 +0300

    just test git bash

--- src/main/java/com/ataev/test_kata/StaticInitLesson.java
+++ src/main/java/com/ataev/test_kata/StaticInitLesson.java
@@ -1,17 +1,12 @@
 package com.ataev.test_kata;
 
 public class StaticInitLesson {
-    static {
-        i = 10;
-    }
+    static { i = 10; }
     static int i = 0;
-    static {
-        ++i;
-    }
+    static { ++i; }
 
     public static void main(String[] args) {
         new StaticInitLesson();
         System.out.println(i);
-
     }
 }
