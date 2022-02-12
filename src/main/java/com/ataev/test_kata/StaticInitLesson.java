a86b542e0cabd22862128ec61455060a590cceb4
IsmailAtaev <ataewisma@gmail.com>
2022-02-12 23:46:17 +0300

    static value test

---
+++ src/main/java/com/ataev/test_kata/StaticInitLesson.java
@@ -0,0 +1,16 @@
+package com.ataev.test_kata;
+
+public class StaticInitLesson {
+    static {
+        i = 10;
+    }
+    static int i = 0;
+    static {
+        ++i;
+    }
+
+    public static void main(String[] args) {
+        new StaticInitLesson();
+        System.out.println(i);
+    }
+}
